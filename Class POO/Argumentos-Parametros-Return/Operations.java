25b7c8151c4a4a813dbed74fd420c13c918ed649
TeamFX-Star <125775318+TeamFX-Star@users.noreply.github.com>
2023-10-03 16:32:32 -0600

    Create Operations.java

---
+++ Class POO/Argumentos-Parametros-Return/Operations.java
@@ -0,0 +1,24 @@
+package ArgumentosYParametros;
+
+public class Operations {
+
+    public float Suma(float a, float b) {
+        float suma = a + b;
+        return suma;
+    }
+
+    public float Resta(float a, float b) {
+        float resta = a - b;
+        return resta;
+    }
+    
+    public float Multiplicacion(float a, float b) {
+        float resta = a * b;
+        return resta;
+    }
+    
+    public float Division(float a, float b) {
+        float resta = a / b;
+        return resta;
+    }
+}
