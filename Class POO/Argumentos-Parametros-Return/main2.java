045a2ebc85a12afb23ce2bca15d1f8a7391fe5a0
TeamFX-Star <125775318+TeamFX-Star@users.noreply.github.com>
2023-10-05 23:41:58 -0600

    Create main2.java

---
+++ Class POO/Argumentos-Parametros-Return/main2.java
@@ -0,0 +1,15 @@
+package ArgumentosYParametros;
+import javax.swing.JOptionPane;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Operationsv2 res = new Operationsv2();
+
+        res.LeerD();
+        res.Suma();
+        res.Resta();
+        res.Division();
+        res.Multiplicacion();
+        res.MostrarRes();
+    }
+}
