4dcb26f53e6e2370fcd8d5bf3bca2db8506b4cf7
TeamFX-Star <125775318+TeamFX-Star@users.noreply.github.com>
2023-10-16 23:34:45 -0600

    Create main.java

---
+++ Constructor/main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Crear una instancia de la clase Metodos utilizando el constructor
+        Metodos metodos = new Metodos();
+        metodos.mostrarDatos();
+    }
+}
